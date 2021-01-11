d183a8680205938ba5899dac1ab99fc25701bfef
Yum <jallygaming@gmail.com>
2021-01-11 21:31:56 +0200

    Обновление 2.1.0.1

---
+++ Vanilla/ManaUsageReplacements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CalamityRuTranslate.Utilities;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityRuTranslate.Vanilla
+{
+    public class ManaUsageReplacements : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (Translation.IsRussianLanguage)
+            {
+                foreach (TooltipLine tooltip in tooltips)
+                {
+                    if (tooltip.Name == "UseMana")
+                    {
+                        string manaUsage = tooltip.text.Split(' ')[1];
+                        if (int.Parse(manaUsage) == 1 || manaUsage.Length > 1 &&
+                            manaUsage[manaUsage.Length - 1] == '1' && manaUsage[manaUsage.Length - 2] != '1')
+                        {
+                            tooltip.text = string.Format(Language.GetTextValue("CommonItemTooltip.NewUsesMana"), manaUsage);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Vanilla/VanillaIL.cs
@@ -0,0 +1,59 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace CalamityRuTranslate.Vanilla
+{
+    public static class VanillaIL
+    {
+        public static void LoadIL()
+        {
+            IL.Terraria.Main.DrawInfoAccs += RU_DrawInfoAccs;
+        }
+
+        public static void UnloadIL()
+        {
+            IL.Terraria.Main.DrawInfoAccs -= RU_DrawInfoAccs;
+        }
+
+        private static void RU_DrawInfoAccs(ILContext il)
+        {
+            ILCursor cursor = new ILCursor(il);
+
+            for (int j = 0; j < 1; j++)
+            {
+                if (!cursor.TryGotoNext(i => i.MatchLdcI4(12))) return;
+            }
+            
+            cursor.Index++;
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldc_I4, 1000);
+            
+            cursor = new ILCursor(il);
+            
+            for (int j = 0; j < 3; j++)
+            {
+                if (!cursor.TryGotoNext(i => i.MatchLdcI4(12))) return;
+            }
+            
+            cursor.Index++;
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldc_I4, 0);
+            
+            cursor = new ILCursor(il);
+            
+            if (!cursor.TryGotoNext(i => i.MatchLdstr("AM"))) return;
+
+            cursor.Index++;
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldstr, "");
+            
+            cursor = new ILCursor(il);
+            
+            if (!cursor.TryGotoNext(i => i.MatchLdstr("PM"))) return;
+
+            cursor.Index++;
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldstr, "");
+        }
+    }
+}
