48268e38dbf0938bc4e4b386f46845826888e99e
Yum <jallygaming@gmail.com>
2021-04-24 16:34:11 +0300

    Обновлена система проверки айди

---
+++ Common/Exceptions/ModTypeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalamityRuTranslate.Common.Exceptions
+{
+    public class ModTypeException : Exception
+    {
+        private string _invalidId { get; }
+        
+        public ModTypeException(string invalidId)
+        {
+            _invalidId = invalidId;
+        }
+        
+        public override string Message => $"Ключ {_invalidId} не найден!\n";
+    }
+}
