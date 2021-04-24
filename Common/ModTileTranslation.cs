5c96d0f362d14790bc0a960042dc9e27c619ac07
Yum <jallygaming@gmail.com>
2021-04-24 16:30:10 +0300

    Добавлена структура для перевода тайлов

---
+++ Common/ModTileTranslation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CalamityRuTranslate.Common
+{
+    public readonly struct ModTileTranslation
+    {
+        public string TileId { get; }
+        public string MapEntryName { get; }
+        public string TranslationKey { get; }
+        public Color TileColor { get; }
+
+        public ModTileTranslation(string tileId, string mapEntryName, Color tileColor, string translationKey)
+        {
+            TileId = tileId;
+            MapEntryName = mapEntryName;
+            TranslationKey = translationKey;
+            TileColor = tileColor;
+        }
+        
+        public ModTileTranslation(string tileId, string mapEntryName, Color tileColor)
+        {
+            TileId = tileId;
+            MapEntryName = mapEntryName;
+            TranslationKey = tileId;
+            TileColor = tileColor;
+        }
+    }
+}
