ee6a12cda207f63df1790f75726d0d47340fb421
Yum <jallygaming@gmail.com>
2021-04-28 16:09:49 +0300

    Подправлен код логики IL

--- Common/ILManager.cs
+++ Common/ILManager.cs
@@ -6,34 +6,34 @@ namespace CalamityRuTranslate.Common
     // Original Code https://github.com/Steviegt6/CataclysmMod
     public static class ILManager
     {
-        private static List<ILEdit> ILEdits;
+        private static List<ILEdit> _ilEdits;
 
         public static void Load()
         {
-            ILEdits = new List<ILEdit>();
+            _ilEdits = new List<ILEdit>();
 
             foreach (Type type in CalamityRuTranslate.Instance.Code.GetTypes())
             {
-                if (type.IsAbstract && type.GetConstructor(new Type[] { }) != null)
+                if (type.IsAbstract || type.GetConstructor(new Type[] { }) == null)
                     continue;
-                
+
                 if (!type.IsSubclassOf(typeof(ILEdit)))
                     continue;
-                
+
                 if(Activator.CreateInstance(type) is ILEdit ilEdit && ilEdit.Autoload())
-                    ILEdits.Add(ilEdit);
+                    _ilEdits.Add(ilEdit);
             }
-            
-            foreach (ILEdit ilEdit in ILEdits)
+
+            foreach (ILEdit ilEdit in _ilEdits)
                 ilEdit.Load();
         }
 
         public static void Unload()
         {
-            foreach (ILEdit ilEdit in ILEdits)
+            foreach (ILEdit ilEdit in _ilEdits)
                 ilEdit.Unload();
 
-            ILEdits = null;
+            _ilEdits = null;
         }
     }
 }
