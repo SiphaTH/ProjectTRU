e3b70ec665dae29c2e32d7ed15ed2baf952e7119
Yum <jallygaming@gmail.com>
2021-05-31 13:46:29 +0300

    Исправлен баг с фарго

--- Mods/Fargowiltas/FargoIL.cs
+++ Mods/Fargowiltas/FargoIL.cs
@@ -1681,15 +1681,4 @@ namespace CalamityRuTranslate.Mods.Fargowiltas
             TranslationUtils.ILTranslate(il, "The Twins have awoken!", "Близнецы пробудились!", 2);
         }
     }
-    
-    public class FargoGlobalItemIL : ILEdit
-    {
-        public override bool Autoload() => ModsCall.Fargo != null && TranslationUtils.IsRussianLanguage;
-
-        public override void Load() => FargoGlobalItem.ModifyTooltips += TranslationModifyTooltips;
-
-        public override void Unload() => FargoGlobalItem.ModifyTooltips -= TranslationModifyTooltips;
-
-        private void TranslationModifyTooltips(ILContext il) => TranslationUtils.ILTranslate(il, "[c/ff0000:Eternity Mode:] Reduced attack speed by 25%", "[c/ff0000:Режим Вечности:] Скорость атаки снижена на 25%");
-    }
 }
