ac02ca9f8eafe5822d4d1c637ae3f2e4b2ee2852
Yum <jallygaming@gmail.com>
2022-09-26 13:20:43 +0300

    Немного перевода Фарго и Соулс

--- Vanilla/MonoMod/UIKeybindingListItemPatch.cs
+++ Vanilla/MonoMod/UIKeybindingListItemPatch.cs
@@ -48,11 +48,11 @@ public class UIKeybindingListItemPatch : ContentTranslation, ILoadableContent
             "FargowiltasSouls: Freeze" => "FargowiltasSouls: Заморозка",
             "FargowiltasSouls: Turn Gold" => "FargowiltasSouls: Превращение в золото",
             "FargowiltasSouls: Throw Smoke Bomb" => "FargowiltasSouls: Дымовая бомба",
-            "FargowiltasSouls: Special Dash" => "FargowiltasSouls: Special Dash",
+            "FargowiltasSouls: Special Dash" => "FargowiltasSouls: Особый рывок",
             "FargowiltasSouls: Mutant Bomb" => "FargowiltasSouls: Бомба Мутанта",
             "FargowiltasSouls: Open Soul Toggler" => "FargowiltasSouls: Открыть настройки эффектов душ",
             "FargowiltasSouls: Precision Movement" => "FargowiltasSouls: Точное передвижение",
-            "FargowiltasSouls: Magical Cleanse" => "FargowiltasSouls: Magical Cleanse",
+            "FargowiltasSouls: Magical Cleanse" => "FargowiltasSouls: Магическое очищение",
             "FargowiltasSouls: Frigid Spell" => "FargowiltasSouls: Frigid Spell",
             "FargowiltasSouls: Debuff Install" => "FargowiltasSouls: Debuff Install",
             _ => orig.Invoke(self)
