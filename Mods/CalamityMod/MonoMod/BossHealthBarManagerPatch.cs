2a2ae5921c7637b44f35e210cb224fa6d6356676
Yum <jallygaming@gmail.com>
2022-10-25 22:21:32 +0300

    Правки в переводе Каламити

--- Mods/CalamityMod/MonoMod/BossHealthBarManagerPatch.cs
+++ Mods/CalamityMod/MonoMod/BossHealthBarManagerPatch.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Linq;
 using System.Reflection;
+using CalamityMod;
+using CalamityMod.NPCs.ExoMechs.Apollo;
+using CalamityMod.NPCs.ExoMechs.Artemis;
 using CalamityMod.UI;
 using CalamityRuTranslate.Common;
 using CalamityRuTranslate.Common.Utilities;
 using CalamityRuTranslate.Core.MonoMod;
 using MonoMod.Cil;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace CalamityRuTranslate.Mods.CalamityMod.MonoMod;
 
@@ -19,14 +26,34 @@ public class BossHealthBarManagerPatch : ILPatcher
     };
 }
 
-public class AttemptToAddBar : ILPatcher
+public class AttemptToAddBar : OnPatcher
 {
     public override bool AutoLoad => ModsCall.Calamity != null && TranslationHelper.IsRussianLanguage;
     
     public override MethodInfo ModifiedMethod => typeof(BossHealthBarManager).GetCachedMethod(nameof(BossHealthBarManager.AttemptToAddBar));
 
-    public override ILContext.Manipulator PatchMethod { get; } = il =>
+    private delegate void AttemptToAddBarDelegate(int index);
+
+    public override Delegate Delegate => Translation;
+
+    private void Translation(AttemptToAddBarDelegate orig, int index)
     {
-        TranslationHelper.ModifyIL(il, "XS-01 Artemis and XS-03 Apollo", "ВП-01 Артемида и ВП-03 Аполлон");
-    };
+        if (BossHealthBarManager.Bars.Count >= BossHealthBarManager.MaximumBars)
+            return;
+        
+        NPC npc = Main.npc[index];
+        bool flag = npc.active && npc.life > 0 && BossHealthBarManager.Bars.All((Func<BossHealthBarManager.BossHPUI, bool>) (b => b.NPCIndex != index)) && !npc.Calamity().ShouldCloseHPBar;
+        string overridingName = null;
+        
+        if (npc.type == ModContent.NPCType<Artemis>())
+            flag = false;
+        
+        if (npc.type == ModContent.NPCType<Apollo>())
+            overridingName = "ВП-01 Артемида и ВП-03 Аполлон";
+        
+        if (!flag)
+            return;
+        
+        BossHealthBarManager.Bars.Add(new BossHealthBarManager.BossHPUI(index, overridingName));
+    }
 }
